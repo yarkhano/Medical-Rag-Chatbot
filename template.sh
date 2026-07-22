5c2f202010ab517e360c04c16128faea2a6d6079
yarkhano <muhammadyarkhan31@gmail.com>
2026-07-22 12:27:56 +0500

    initial files are creaed

---
+++ template.sh
@@ -0,0 +1,12 @@
+#creating diretories
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch requirements.txt
+touch setup.py
+touch app.py
+touch research/trials.ipynb
